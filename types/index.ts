f8b5b8c2de437140ed98f51d283c1d99a7632ebe
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2025-05-06 17:36:27 +0100

    modified:   types/index.ts

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,3 @@
 // src/types/index.ts (or src/types/common.ts, etc.)
 
 export type FetchStatus = "idle" | "loading" | "succeeded" | "failed";
-
-// You can add other shared types here later if needed
