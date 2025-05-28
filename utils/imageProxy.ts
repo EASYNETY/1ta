975fcbc5972b5e48f78c45a724cc8d40c409e8a6
User <user@example.com>
2025-05-28 21:03:48 +0100

    modified:   utils/imageProxy.ts

--- utils/imageProxy.ts
+++ utils/imageProxy.ts
@@ -1,3 +1,9 @@
+const BASE_URL = "http://34.249.241.206:5000";
+
 export function getProxiedImageUrl(imageUrl: string): string {
-    return `/api/images?imageUrl=${encodeURIComponent(imageUrl)}`;
+	// If it starts with "http://" or "https://", assume it's absolute
+	const isAbsolute = /^https?:\/\//i.test(imageUrl);
+
+	const fullUrl = isAbsolute ? imageUrl : `${BASE_URL}${imageUrl}`;
+	return `/api/images?imageUrl=${encodeURIComponent(fullUrl)}`;
 }
