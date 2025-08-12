17c208e6da88cf7ae5efe2997feb6c47d4280544
EASYNETY <israeladeyeyee@gmail.com>
2025-08-12 18:28:21 +0100

    change effect

--- components/SocketInit.tsx
+++ components/SocketInit.tsx
@@ -1,7 +1,23 @@
 "use client";
-import useSocketListeners from "@/hooks/useSocketListeners";
+
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import socket from "@/lib/socket"; // should export the connected socket.io client
+import { AnyAction } from "@reduxjs/toolkit";
 
 export default function SocketInit() {
-  useSocketListeners();
-  return null;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Listen for server state updates and send them straight into Redux
+    socket.on("state-update", (action: AnyAction) => {
+      dispatch(action);
+    });
+
+    return () => {
+      socket.off("state-update");
+    };
+  }, [dispatch]);
+
+  return null; // nothing visual
 }
